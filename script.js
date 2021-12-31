c254fd913d5f1e4254a19f76ac179262271a4923
TranNgocTuan1 <tomastuan12@gmail.com>
2021-12-31 14:04:39 +0100

    Verze 3

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 $(document).ready(function(){
 
+    $(".cart").click(function(){
+
+        // Perform your action on click here, like redirecting to a new url
+        window.location='kosik.html';
+    });
    
 });
